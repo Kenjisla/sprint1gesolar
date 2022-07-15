2af88c6a3960fc9abbe3011900e05fe606a08e81
Leonardo Santana <82040303+leosantanadev@users.noreply.github.com>
2022-07-15 18:24:08 -0300

    App

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
+import { FaWhatsapp } from "react-icons/fa";
 import { HomePageFormWhite } from "../components/Forms/HomePageFormWhite";
 import { HeroSection } from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
 import { HomeObjectOne, HomeObjectThree, HomeObjectTwo } from "../components/InfoSection/data";
-import { WhatsappButton } from "../components/WhatsappButton";
 
 export default function Home() {  
   return (
@@ -163,7 +163,18 @@ export default function Home() {
 
       <InfoSection {...HomeObjectThree}/>
 
-      <WhatsappButton />
+      <a
+        className="flex items-center justify-center gap-3 px-4 py-2 fixed bottom-4 right-4 bg-[#2ecc71] text-neutral-50 z-50 rounded-full md:bottom-6 hover:bg-[#29b765] hover:text-neutral-300 focus:outline-none transition-colors duration-200"
+        href="https://api.whatsapp.com/send?phone=551151981055" 
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span className="flex items-center justify-center">
+            <FaWhatsapp size={33}/>
+        </span>
+
+        <span>Orçamento via whatsapp</span>
+      </a>
     </>
   );
 }
