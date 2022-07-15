0733d7f609cf092246129619c1db018ec4ccca22
Leonardo Santana <82040303+leosantanadev@users.noreply.github.com>
2022-07-15 18:55:50 -0300

    App

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         subtitle="Produza sua própria energia trocando o custo da sua conta 
         de luz para pagar o investimento no sistema."
         buttonLink="/#calculator"
-        buttonLabel="Começar agora"
+        buttonLabel="Simular agora"
         imgSrc="/hero-image.jpg"
       />
 
@@ -164,7 +164,7 @@ export default function Home() {
       <InfoSection {...HomeObjectThree}/>
 
       <a
-        className="flex items-center justify-center gap-3 px-4 py-2 fixed bottom-4 right-4 bg-[#2ecc71] text-neutral-50 z-50 rounded-full md:bottom-6 hover:bg-[#29b765] hover:text-neutral-300 focus:outline-none transition-colors duration-200"
+        className="flex items-center justify-center gap-3 px-4 py-2 max-w-[200px] fixed bottom-4 right-4 bg-[#2ecc71] text-neutral-50 z-50 rounded-full md:max-w-full md:bottom-6 hover:bg-[#29b765] hover:text-neutral-300 focus:outline-none transition-colors duration-200"
         href="https://api.whatsapp.com/send?phone=551151981055" 
         target="_blank"
         rel="noreferrer"
