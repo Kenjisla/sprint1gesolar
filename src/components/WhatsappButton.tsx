43fd8e22a40002ea90682f9be3705d3a316992a5
Leonardo Santana <82040303+leosantanadev@users.noreply.github.com>
2022-07-15 18:21:46 -0300

    App

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -3,14 +3,16 @@ import { FaWhatsapp } from 'react-icons/fa'
 export function WhatsappButton(){
     return(
         <a
-            className="fixed w-14 h-14 bottom-6 right-4 bg-[#2ecc71] text-neutral-50 z-50 rounded-full hover:bg-[#29b765] hover:text-neutral-300 focus:outline-none transition-colors duration-200"
+            className="flex items-center justify-center gap-3 px-4 py-2 fixed bottom-4 right-4 bg-[#2ecc71] text-neutral-50 z-50 rounded-full md:bottom-6 hover:bg-[#29b765] hover:text-neutral-300 focus:outline-none transition-colors duration-200"
             href="https://api.whatsapp.com/send?phone=551151981055" 
             target="_blank"
             rel="noreferrer"
         >
-            <span className="w-full h-full flex items-center justify-center">
+            <span className="flex items-center justify-center">
                 <FaWhatsapp size={33}/>
             </span>
+
+            <span>Orçamento via whatsapp</span>
         </a>
     )
 }
