24fa52a6c08f239799f1571ddda9411bf8fa034b
Leonardo Santana <82040303+leosantanadev@users.noreply.github.com>
2023-02-16 16:29:02 -0300

    changed whatsappButton number

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -171,7 +171,7 @@ export default function Home() {
 
       <a
         className="flex items-center justify-center gap-3 px-4 py-2 max-w-[200px] fixed bottom-4 right-4 bg-[#2ecc71] text-neutral-50 z-50 rounded-full md:max-w-full md:bottom-6 hover:bg-[#29b765] hover:text-neutral-300 focus:outline-none transition-colors duration-200"
-        href="https://api.whatsapp.com/send?phone=5511941279117" 
+        href="https://api.whatsapp.com/send?phone=551151984410" 
         target="_blank"
         rel="noreferrer"
       >
