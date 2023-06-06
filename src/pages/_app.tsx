c09e4ab8309f1177b113fe4562aa79cb95c282b6
leosantanadev <ls07062005@gmail.com>
2023-06-05 22:28:23 -0300

    tirando duplicação do navbar na tela calculadora de energia solar 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,14 @@ import "../styles/globals.css"
 import type { AppProps } from 'next/app'
 import { CalculatorProvider } from "../hooks/useCalculator";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const hasOtherNavbar = router.asPath === '/calculadora-de-energia-solar2'
+
   return (
     <>
       <Head>
@@ -16,9 +21,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="Venda, instalação e manutenção de energia Solar. Produza sua própria energia e pare de pagar conta de luz."></meta>
       </Head>
 
-      <header>
-        <Navbar />
-      </header>      
+      {!hasOtherNavbar && (
+        <header>
+          <Navbar />
+        </header> 
+      )}     
       
       <main>
           <CalculatorProvider>
